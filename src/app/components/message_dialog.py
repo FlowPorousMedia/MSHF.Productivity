f3657d78892385b3a043d2abd0fdd85ddbd24518
erythrocyte <marseille.tatar@yahoo.com>
2025-08-26 00:43:03 +0300

    fix message title color

--- src/app/components/message_dialog.py
+++ src/app/components/message_dialog.py
@@ -9,17 +9,38 @@ from src.app.models.message_type import MessageType
 def get_message_dialog(
     dialog_id, title, message, type: MessageType.INFO, buttons=["OK"]
 ):
+    title_colors = {
+        MessageType.INFO: "blue",
+        MessageType.WARNING: "orange",
+        MessageType.ERROR: "red",
+    }
+
     icons = {
-        MessageType.INFO: html.I(className="fa-solid fa-circle-info"),
-        MessageType.WARNING: html.I(className="fa-solid fa-triangle-exclamation"),
+        MessageType.INFO: html.I(
+            className="fa-solid fa-circle-info",
+            style={"color": title_colors[MessageType.INFO], "margin-right": "8px"},
+        ),
+        MessageType.WARNING: html.I(
+            className="fa-solid fa-triangle-exclamation",
+            style={"color": title_colors[MessageType.WARNING], "margin-right": "8px"},
+        ),
         MessageType.ERROR: html.I(
-            className="fas fa-circle-xmark", style={"color": "red", "margin-right": "8px"}
+            className="fas fa-circle-xmark",
+            style={"color": title_colors[MessageType.ERROR], "margin-right": "8px"},
         ),
     }
+
     return dbc.Modal(
         [
             dbc.ModalHeader(
-                dbc.ModalTitle([icons.get(type, ""), title]),
+                dbc.ModalTitle(
+                    [
+                        icons.get(type, ""),
+                        html.Span(
+                            title, style={"color": title_colors.get(type, "black")}
+                        ),
+                    ]
+                ),
                 close_button=False,  # disable the "X"
             ),
             dbc.ModalBody(message),
