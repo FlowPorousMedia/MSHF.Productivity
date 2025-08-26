eca6990b5d19a33dcf6da56a83229adba59412ef
erythrocyte <marseille.tatar@yahoo.com>
2025-08-26 21:06:53 +0300

    all done

--- src/app/components/about_modal.py
+++ src/app/components/about_modal.py
@@ -7,7 +7,7 @@ from src.app._version import SOFTWARE_TITLE, USER_VERSION
 def create_about_modal():
     return dbc.Modal(
         [
-            dbc.ModalHeader(f"About {SOFTWARE_TITLE}"),
+            dbc.ModalHeader(f"About {SOFTWARE_TITLE} (v.{USER_VERSION})"),
             dbc.ModalBody(
                 [
                     html.H4(
@@ -18,10 +18,83 @@ def create_about_modal():
                             " is a powerful Productivity Evaluation Tool for Multistage Fractured Horizontal Wells (MSHF)",
                         ]
                     ),
-                    html.P(f"Version {USER_VERSION}"),
-                    # html.P(
-                    #     "Analytics Pro is a powerful data analysis and visualization tool designed for professionals."
-                    # ),
+                    html.P(
+                        "This software tool enables petroleum engineers to rapidly forecast production rates for multistage "
+                        "fractured horizontal wells. It utilizes analytical modeling to perform steady-state flow analysis, "
+                        "providing a quick and efficient solution for well performance evaluation."
+                    ),
+                    # Authors section
+                    html.H5(
+                        "Авторы", style={"margin-top": "20px", "margin-bottom": "10px"}
+                    ),
+                    html.Ul(
+                        [
+                            html.Li(
+                                [
+                                    html.A(
+                                        "Марсель Хамидуллин",
+                                        href="https://www.researchgate.net/profile/Marsel-Khamidullin",
+                                        target="_blank",
+                                    )
+                                ]
+                            ),
+                            html.Li(
+                                [
+                                    html.A(
+                                        "Константин Поташев",
+                                        href="https://www.researchgate.net/profile/Konstantin-Potashev",
+                                        target="_blank",
+                                    )
+                                ]
+                            ),
+                        ]
+                    ),
+                    # License section
+                    html.H5(
+                        "Лицензия",
+                        style={"margin-top": "20px", "margin-bottom": "10px"},
+                    ),
+                    html.P(
+                        [
+                            "© ",
+                            html.A(
+                                "FlowPorousMedia",
+                                href="https://github.com/FlowPorousMedia/",
+                                target="_blank",
+                            ),
+                            ", 2025. Все права защищены",
+                        ]
+                    ),
+                    html.P(
+                        [
+                            "Этот проект распространяется под лицензией ",
+                            html.A(
+                                "MIT",
+                                href="https://github.com/FlowPorousMedia/MSHF.Productivity/blob/main/LICENSE",
+                                target="_blank",
+                            ),
+                        ]
+                    ),
+                    # Support section with GitHub Issues link
+                    html.H5(
+                        "Поддержка",
+                        style={"margin-top": "20px", "margin-bottom": "10px"},
+                    ),
+                    html.P(
+                        [
+                            "Для получения поддержки, пожалуйста, создайте запрос в ",
+                            html.A(
+                                "GitHub Issues",
+                                href="https://github.com/FlowPorousMedia/MSHF.Productivity/issues",
+                                target="_blank",
+                            ),
+                            ".",
+                        ]
+                    ),
+                    html.Small(
+                        "Мы отвечаем на вопросы и учитываем предложения по улучшению",
+                        style={"font-style": "italic", "color": "gray"},
+                    ),
                     # html.P("Features include:"),
                     # html.Ul(
                     #     [
