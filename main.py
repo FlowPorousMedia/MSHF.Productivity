91e2957d22d58defed88d19eee30529bec1ff555
erythrocyte <marseille.tatar@yahoo.com>
2025-10-14 21:39:49 +0300

    remove unused code

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import pkgutil, importlib
-
-if not hasattr(pkgutil, "find_loader"):
-    pkgutil.find_loader = importlib.util.find_spec
-
-
 from src.app.server import app  # Import Dash instance
 
 if __name__ == "__main__":
